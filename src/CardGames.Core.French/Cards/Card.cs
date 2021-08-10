e55b0b371b64471db5fc445ce288031d70d0d739
Guy Buss <guysbuss@gmail.com>
2021-08-10 23:15:45 +0200

    Implement IEquatable n french card for performance

--- src/CardGames.Core.French/Cards/Card.cs
+++ src/CardGames.Core.French/Cards/Card.cs
@@ -1,8 +1,9 @@
 ﻿using CardGames.Core.French.Cards.Extensions;
+using System;
 
 namespace CardGames.Core.French.Cards
 {
-    public readonly struct Card
+    public readonly struct Card : IEquatable<Card>
     {
         public Suit Suit { get; }
 
@@ -25,5 +26,25 @@ namespace CardGames.Core.French.Cards
 
         public override string ToString() 
             => this.ToShortString();
+
+        // The rest of the struct is solemnly for performance. Although a struct already comes with an implementation of 
+        // equality, it is based on a generic implementation using reflection. To acheive (much) better performance, it is 
+        // recommended to overwrite to implementation with a specific one, and that results in hte boilderpalte below. See, e.g.,
+        // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/how-to-define-value-equality-for-a-type
+        public override bool Equals(object obj)
+            => obj is Card other && Equals(other);
+
+        public bool Equals(Card other)
+            => Suit == other.Suit && Symbol == other.Symbol;
+
+        public static bool operator ==(Card left, Card right) 
+            => left.Equals(right);
+
+        public static bool operator !=(Card left, Card right) 
+            => !(left == right);
+
+        // This is the standard mapping of suits and values to 1..52, which is also used in the SerilaizationExtensions.
+        public override int GetHashCode()
+            => (int)Suit * 13 + Value - 2;
     }
 }
