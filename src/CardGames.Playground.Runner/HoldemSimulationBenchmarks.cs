629a0547e3107f1d70a70285850fe310651202c8
EluciusFTW <guysbuss@gmail.com>
2023-11-25 10:29:42 +0100

    Upgraded to .NET8

--- src/CardGames.Playground.Runner/HoldemSimulationBenchmarks.cs
+++ src/CardGames.Playground.Runner/HoldemSimulationBenchmarks.cs
@@ -2,27 +2,36 @@
 using CardGames.Poker.Simulations.Holdem;
 using CardGames.Core.French.Cards.Extensions;
 
-namespace CardGames.Poker.Benchmarks
-{
-    [MemoryDiagnoser]
-    public class HoldemSimulationBenchmarks
-    {
-        [Benchmark]
-        public static HoldemSimulationResult HoldemHeadsUp100Hands()
-            => new HoldemSimulation()
-                .WithPlayer("James", "8s 6d".ToCards())
-                .WithPlayer("Jimmy", "Ad Kd".ToCards())
-                .WithFlop("3h 6c Qd".ToCards())
-                .SimulateWithFullDeck(100);
+namespace CardGames.Poker.Benchmarks;
 
-        [Benchmark]
-        public static HoldemSimulationResult HoldemFiveHanded100Hands()
-            => new HoldemSimulation()
-                .WithPlayer("Jill", "Qh Td".ToCards())
-                .WithPlayer("Jonas", "8s 9d".ToCards())
-                .WithPlayer("James", "2h 2c".ToCards())
-                .WithPlayer("John", "Ks Js".ToCards())
-                .WithPlayer("Jimmy", "Ad Kd".ToCards())
-                .SimulateWithFullDeck(100);
-    }
+[MemoryDiagnoser]
+public class HoldemSimulationBenchmarks
+{
+    [Benchmark]
+    public HoldemSimulationResult Holdem_HeadsUp_100_Hands()
+        => HeadsUpSimulation().SimulateWithFullDeck(100);
+
+    [Benchmark]
+    public HoldemSimulationResult Holdem_HeadsUp_1000_Hands()
+        => HeadsUpSimulation().SimulateWithFullDeck(1000);
+
+    [Benchmark]
+    public HoldemSimulationResult Holdem_FiveHanded_100_Hands()
+        => FiveHandedSimulation().SimulateWithFullDeck(100);
+
+    [Benchmark]
+    public HoldemSimulationResult Holdem_FiveHanded_1000_Hands()
+        => FiveHandedSimulation().SimulateWithFullDeck(1000);
+
+    private HoldemSimulation HeadsUpSimulation()
+        => new HoldemSimulation()
+            .WithPlayer("James", "8s 6d".ToCards())
+            .WithPlayer("Jimmy", "Ad Kd".ToCards())
+            .WithFlop("3h 6c Qd".ToCards());
+
+    private HoldemSimulation FiveHandedSimulation()
+        => new HoldemSimulation()
+            .WithPlayer("James", "8s 6d".ToCards())
+            .WithPlayer("Jimmy", "Ad Kd".ToCards())
+            .WithFlop("3h 6c Qd".ToCards());
 }
--- src/CardGames.Playground.Runner/Program.cs
+++ src/CardGames.Playground.Runner/Program.cs
@@ -1,72 +1,13 @@
 ﻿using System;
-using System.Linq;
 using BenchmarkDotNet.Running;
-using CardGames.Poker.Simulations.Holdem;
-using CardGames.Core.Extensions;
-using CardGames.Core.French.Cards.Extensions;
-using CardGames.Poker.Simulations.Stud;
 
-namespace CardGames.Poker.Benchmarks
+namespace CardGames.Poker.Benchmarks;
+
+public class Program
 {
-    public class Program
+    static void Main()
     {
-        static void Main()
-        {
-            //var results = RunStudSimulation(10000);
-
-            //PrintWinPercentages(results);
-            //PrintHandDistributions(results);
-
-            BenchmarkRunner.Run<HoldemSimulationBenchmarks>();
-            Console.ReadKey();
-
-            // Console.ReadKey();
-        }
-
-        private static HoldemSimulationResult RunHoldemSimulation(int nrOfHands)
-           => new HoldemSimulation()
-               .WithPlayer("John", "Js Jd".ToCards())
-               .WithPlayer("Jeremy", "8s 6d".ToCards())
-               .WithPlayer("Jarvis", "Ad".ToCards())
-               .WithFlop("8d 8h 4d".ToCards())
-               .SimulateWithFullDeck(nrOfHands);
-
-        private static StudSimulationResult RunStudSimulation(int nrOfHands)
-            => new SevenCardStudSimulation()
-                .WithPlayer(
-                    new StudPlayer("John")
-                        .WithHoleCards("Js Jd".ToCards())
-                        .WithBoardCards("Qc".ToCards()))
-                .WithPlayer(
-                    new StudPlayer("Jeremy")
-                        .WithHoleCards("3s 4s".ToCards())
-                        .WithBoardCards("7s".ToCards()))
-                .WithPlayer(
-                    new StudPlayer("Jarvis")
-                        .WithBoardCards("Tc".ToCards()))
-                .WithDeadCards("Qs Qh Jc 6s".ToCards())
-                .Simulate(nrOfHands);
-
-        private static void PrintWinPercentages(StudSimulationResult result)
-            => result
-                .GroupByWins()
-                .OrderByDescending(player => player.WinPercentage)
-                .ForEach(player =>
-                {
-                    Console.WriteLine($"{player.Name} won {player.Wins} hands => {player.WinPercentage:P2}");
-                });
-
-        private static void PrintHandDistributions(StudSimulationResult results)
-            => results
-                .AllMadeHandDistributions()
-                .ForEach(distribution =>
-                {
-                    Console.WriteLine();
-                    Console.WriteLine($"{distribution.Key} made:");
-                    distribution.Value.ForEach(typeDistribution =>
-                    {
-                        Console.WriteLine($" * {typeDistribution.Type} - {typeDistribution.Occurences} times ({typeDistribution.Frequency:P2})");
-                    });
-                });
+        BenchmarkRunner.Run<HoldemSimulationBenchmarks>();
+        Console.ReadKey();
     }
 }
